ad02480d89e3839fbd4e3c834ff22144f651d708
willspot <spelumi03@gmail.com>
2024-12-13 06:30:49 -0800

    add, subtract, multiply

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = 10 + 5
+subtraction = 10 - 5
+multiplication = 10 * 5
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
