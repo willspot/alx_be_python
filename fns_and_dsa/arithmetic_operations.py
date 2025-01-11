7b7921eb13eb20e387416571a337381ba0c4b182
Alabi-williams samuel <49067163+willspot@users.noreply.github.com>
2025-01-11 02:29:47 -0800

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str) -> float:
+def perform_operation(num1, num2, operation) -> float:
    
     if operation == 'add':
         return num1 + num2
