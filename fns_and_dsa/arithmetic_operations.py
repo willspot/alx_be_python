14cf59209abdf5172e6bb43171238f1be2ad6d9f
Alabi-williams samuel <49067163+willspot@users.noreply.github.com>
2025-01-11 02:35:00 -0800

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,15 @@
 def perform_operation(num1: float, num2: float, operation: str):
+    """
+    Perform basic arithmetic operations based on the given operation.
     
+    Parameters:
+    - num1: The first number (float).
+    - num2: The second number (float).
+    - operation: A string that specifies the operation ('add', 'subtract', 'multiply', 'divide').
+
+    Returns:
+    - The result of the operation (float), or an error message for invalid operations or division by zero.
+    """
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
